1e31cc686c055b6e27c27e9de734d17cd47c8b41
Marcin Młodziński <marcin.mlodzinski00@gmail.com>
2022-05-29 17:29:00 +0200

    improve code formatting

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -110,46 +110,46 @@ const struct lfs_config cfg = {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-/* Configure the peripherals common clocks */
-  PeriphCommonClock_Config();
+    /* Configure the peripherals common clocks */
+    PeriphCommonClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_I2C2_Init();
-  MX_LCD_Init();
-  MX_QUADSPI_Init();
-  MX_SAI1_Init();
-  MX_SPI2_Init();
-  MX_USART2_UART_Init();
-  MX_RTC_Init();
-  MX_IWDG_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_I2C2_Init();
+    MX_LCD_Init();
+    MX_QUADSPI_Init();
+    MX_SAI1_Init();
+    MX_SPI2_Init();
+    MX_USART2_UART_Init();
+    MX_RTC_Init();
+    MX_IWDG_Init();
+    /* USER CODE BEGIN 2 */
     BSP_LCD_GLASS_Init();
     BSP_LCD_GLASS_Clear();
     BSP_QSPI_Init();
@@ -175,8 +175,8 @@ int main(void)
 
     int menu_position = 0;
     int menu_select = 0;
-    bool erase_flag=false;
-    bool new_data=false;
+    bool erase_flag = false;
+    bool new_data = false;
     int set_time_tmp = -1;
 
     char record_file_name[10];
@@ -184,15 +184,15 @@ int main(void)
 
     if (!lfs_mount(&lfs, &cfg))
     {
-    	BSP_LCD_GLASS_DisplayString((uint8_t *)"FS OK");
+        BSP_LCD_GLASS_DisplayString((uint8_t *)"FS OK");
         readFile(&lfs, &file, "number_of_records", &number_of_records, sizeof(number_of_records));
         readFile(&lfs, &file, "delay_between_saves", &delay_between_saves, sizeof(delay_between_saves));
     }
     else
     {
-    	BSP_LCD_GLASS_DisplayString((uint8_t *)"FS NEW");
-    	lfs_format(&lfs, &cfg);
-    	lfs_mount(&lfs, &cfg);
+        BSP_LCD_GLASS_DisplayString((uint8_t *)"FS NEW");
+        lfs_format(&lfs, &cfg);
+        lfs_mount(&lfs, &cfg);
         writeFile(&lfs, &file, "number_of_records", &number_of_records, sizeof(number_of_records));
         writeFile(&lfs, &file, "delay_between_saves", &delay_between_saves, sizeof(delay_between_saves));
     }
@@ -201,10 +201,10 @@ int main(void)
 
     sprintf(text, "Uzyj joysticka aby nawigowac      ");
 
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1)
     {
         now = HAL_GetTick();
@@ -232,40 +232,42 @@ int main(void)
                 case 2:
                     if (number_of_records)
                     {
-                    	if(new_data){
-                    		i=0;
-                    		new_data=false;
-                    	}
+                        if (new_data)
+                        {
+                            i = 0;
+                            new_data = false;
+                        }
                         switch (joy_state)
                         {
                         case JOY_RIGHT:
-                        	if(!erase_flag)
-                        	                        	{
-                        		erase_flag=true;
-                        		                        	i=0;
-                        	                        	}
+                            if (!erase_flag)
+                            {
+                                erase_flag = true;
+                                i = 0;
+                            }
 
-                        	break;
+                            break;
                         case JOY_LEFT:
-                        	if(erase_flag)
-                        	{
-                        		erase_flag=false;
-                        		i=0;
-                        	}
-                        	break;
+                            if (erase_flag)
+                            {
+                                erase_flag = false;
+                                i = 0;
+                            }
+                            break;
                         case JOY_SEL:
-                        	if(erase_flag){
-                        		lfs_unmount(&lfs);
-                        		lfs_format(&lfs, &cfg);
-                        		lfs_mount(&lfs, &cfg);
-                        		number_of_records=0;
-                        		writeFile(&lfs, &file, "number_of_records", &number_of_records, sizeof(number_of_records));
-                        		writeFile(&lfs, &file, "delay_between_saves", &delay_between_saves, sizeof(delay_between_saves));
-                        		read_record_number=1;
-                        		erase_flag=false;
-                        	}
+                            if (erase_flag)
+                            {
+                                lfs_unmount(&lfs);
+                                lfs_format(&lfs, &cfg);
+                                lfs_mount(&lfs, &cfg);
+                                number_of_records = 0;
+                                writeFile(&lfs, &file, "number_of_records", &number_of_records, sizeof(number_of_records));
+                                writeFile(&lfs, &file, "delay_between_saves", &delay_between_saves, sizeof(delay_between_saves));
+                                read_record_number = 1;
+                                erase_flag = false;
+                            }
 
-                        	break;
+                            break;
                         case JOY_UP:
                             read_record_number++;
                             if (read_record_number > number_of_records)
@@ -286,18 +288,19 @@ int main(void)
                             break;
                         }
                         joy_state = JOY_NONE;
-                        if(!erase_flag){
-                        		sprintf(record_file_name, "record%d", read_record_number);
-                        		readFile(&lfs, &file, record_file_name, &text, sizeof(text));
+                        if (!erase_flag)
+                        {
+                            sprintf(record_file_name, "record%d", read_record_number);
+                            readFile(&lfs, &file, record_file_name, &text, sizeof(text));
                         }
                         else
                         {
-                        	sprintf(text, "Potwierdz usuniecie danych      ");
+                            sprintf(text, "Potwierdz usuniecie danych      ");
                         }
                     }
                     else
                     {
-                    	new_data=true;
+                        new_data = true;
                         sprintf(text, "Brak zapisanych danych      ");
                     }
                     break;
@@ -634,93 +637,91 @@ int main(void)
         }
 
         HAL_IWDG_Refresh(&hiwdg);
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Configure the main internal regulator output voltage
+     */
+    if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  /** Configure LSE Drive Capability
-  */
-  HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+    /** Configure LSE Drive Capability
+     */
+    HAL_PWR_EnableBkUpAccess();
+    __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI
-                              |RCC_OSCILLATORTYPE_LSE;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 1;
+    RCC_OscInitStruct.PLL.PLLN = 10;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 /**
-  * @brief Peripherals Common Clock Configuration
-  * @retval None
-  */
+ * @brief Peripherals Common Clock Configuration
+ * @retval None
+ */
 void PeriphCommonClock_Config(void)
 {
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the peripherals clock
-  */
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI1;
-  PeriphClkInit.Sai1ClockSelection = RCC_SAI1CLKSOURCE_PLLSAI1;
-  PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
-  PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
-  PeriphClkInit.PLLSAI1.PLLSAI1N = 8;
-  PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV7;
-  PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
-  PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
-  PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the peripherals clock
+     */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI1;
+    PeriphClkInit.Sai1ClockSelection = RCC_SAI1CLKSOURCE_PLLSAI1;
+    PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
+    PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+    PeriphClkInit.PLLSAI1.PLLSAI1N = 8;
+    PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV7;
+    PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
+    PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
+    PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -860,10 +861,11 @@ void menu(int *menu_position, char *text, int *menu_select, int *i, bool erase_f
         break;
 
     case JOY_LEFT:
-    	if(!erase_flag){
-        (*menu_select) = 0;
-        (*i) = 0;
-    	}
+        if (!erase_flag)
+        {
+            (*menu_select) = 0;
+            (*i) = 0;
+        }
         break;
 
     case JOY_SEL:
@@ -878,33 +880,33 @@ void menu(int *menu_position, char *text, int *menu_select, int *i, bool erase_f
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1)
     {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
